e5a5eaa2664e574be5d24a793229a145b97cc0b4
inconsiderate <meiskant@gmail.com>
2020-08-22 23:08:12 -0700

    Update stadia_scrape.js

--- stadia_scrape.js
+++ stadia_scrape.js
@@ -1,7 +1,6 @@
-
 var game = {
     "achievements": [],
-    "title": 'SteamWorld Quest: Hand of Gilgamech',
+    "title": 'insert_title_here',
 };
 
 var achievementElements = document.getElementsByClassName("h6J22d cWe8yc QAAyWd");
@@ -14,13 +13,15 @@ for (var a = 0; a < achievementElements.length; a++) {
         "image": ''
     };
     var achievementNode = achievementElements.item(a).children;
+    console.log(achievementNode[1].children[0].children);
 
     achievement['image'] = achievementNode[0].firstChild.style.backgroundImage.slice(4, -1).replace(/["']/g, "");
-    achievement['name'] = achievementNode[1].children[0].children[0].innerText;
-    achievement['description'] = achievementNode[1].children[0].children[0].innerText;
+
+    achievement['name'] =  achievementNode[1].children[0].children[0].innerText ? achievementNode[1].children[0].children[0].innerText : achievementNode[1].children[0].children[1].innerText;
+
+    achievement['description'] = achievementNode[1].children[0].children[0].innerText ? achievementNode[1].children[0].children[0].innerText : achievementNode[1].children[0].children[1].innerText;
 
     game['achievements'].push(achievement);
 }
 
-
-console.log(game);
+copy(game);
