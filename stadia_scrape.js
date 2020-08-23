fbead08c1accf66c31736de26a9d5a0e284c3a38
inconsiderate <meiskant@gmail.com>
2020-08-22 23:08:31 -0700

    Update stadia_scrape.js

--- stadia_scrape.js
+++ stadia_scrape.js
@@ -13,12 +13,9 @@ for (var a = 0; a < achievementElements.length; a++) {
         "image": ''
     };
     var achievementNode = achievementElements.item(a).children;
-    console.log(achievementNode[1].children[0].children);
 
     achievement['image'] = achievementNode[0].firstChild.style.backgroundImage.slice(4, -1).replace(/["']/g, "");
-
     achievement['name'] =  achievementNode[1].children[0].children[0].innerText ? achievementNode[1].children[0].children[0].innerText : achievementNode[1].children[0].children[1].innerText;
-
     achievement['description'] = achievementNode[1].children[0].children[0].innerText ? achievementNode[1].children[0].children[0].innerText : achievementNode[1].children[0].children[1].innerText;
 
     game['achievements'].push(achievement);
